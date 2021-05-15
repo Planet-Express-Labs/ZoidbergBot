747378de25268cd99eb3bf1aea550239bc25a285
Liem Eldert <liemeldert@protonmail.com>
2021-05-14 20:46:33 -0400

    Refactored a few folders to the new branding.
    Also migrated strings.

--- zoidbergbot/config.py
+++ zoidbergbot/config.py
@@ -31,109 +31,28 @@ sublime with Ayu doesn't work, for instance.
 ▐▐▐▐▐▐▗▚▘  ▖ ▄▜▝▖▌▘  ▗ ▝                    ▘       ▗▚▚▚▝▞▞▖
 ▘▘▝▝ ▘▌   ▗ ▘▘▝▝▝▝ ▘                 ▘   ▘     ▘     ▘▌▘▘▘▝
 """
+import configparser
+import logging
+import codecs
 
-import discord
-from typing import Optional
-from random import choice
-from discord import Member, Guild, TextChannel, Message, PermissionOverwrite, Role, CategoryChannel, Reaction
-from discord.ext.commands import Bot, Context, check_any, CheckFailure
-from discord import RawReactionActionEvent
-from discord.errors import HTTPException
-from zoidbergbot.state import state
-from zoidbergbot.config import *
-from zoidbergbot.strings import get_string, String
-from zoidbergbot.utilities import generate_id, generate_code
-from datetime import datetime
-from io import BytesIO
-from discord.ext import commands
-import os
-
-__version__ = "1.0 RELEASE CANDIDATE"
+from json import loads
 
 log = logging.getLogger(__name__)
-bot = Bot(
-    command_prefix=BOT_PREFIX
-)
 
-# "!conf" command
-@bot.command()
-async def conf(ctx, *, message=""):
-    if (len(message) != 0) or (ctx.message.attachments != []):
+CONFIG_FILE = "./data/config.ini"
 
-        now = datetime.now()
-        currentTime = now.strftime("%d/%m %H:%M")
-        userID = ctx.message.author.id
-        server = bot.get_guild(GUILD_ID)
-        member = ctx.message.author
-        # TODO: change this to sqlite3. Not here, in beta, but do that.
-        print(now.strftime("%d/%m %H:%M"), file=open("output.txt", "a"))
-        print(userID, file=open("output.txt", "a"))
-        print(server, file=open("output.txt", "a"))
-        print(member, file=open("output.txt", "a"))
+config = configparser.ConfigParser()
+config.read_file(codecs.open(CONFIG_FILE, "r", "utf-8"))
 
-        # Grabs channel IDS from config.
-        # TODO: convert this to use a database per guild id. 
-        channel = bot.get_channel(CHANNEL_ID)
-        logChannel = bot.get_channel(LOG_ID)
+# bot-info.
+BOT_TOKEN = config.get("Bot", "bot_token", fallback=None)
+if not BOT_TOKEN:
+    raise Exception("'bot_token' is missing!")
 
-        # Embed looks fancy.
-        embed = discord.Embed(description=message)
-        print(message, file=open("output.txt", "a"))
-        # Set date/time as footer of embed
-        embed.set_footer(text=currentTime)
-
-        # Image attachments
-        files = []
-        for file in ctx.message.attachments:
-            fp = BytesIO()
-            await file.save(fp)
-            files.append(discord.File(
-                fp, filename=file.filename, spoiler=file.is_spoiler()))
-            imageURL = ctx.message.attachments[0].url
-            embed.set_image(url=imageURL)
-            print("image" + imageURL)
-
-        # Send embed message to channel
-        await channel.send(embed=embed)
-        embed.set_footer(text=currentTime + str(member))
-        await logChannel.send(embed=embed)
-        # TODO: Change to use strings. 
-        await ctx.send("Your message has been sent!")
-
-
-    else: 
-        await ctx.send(CMD_NO_CONTENT)
-    print("--------------------------------------- END CONFESS ---------------------------------------",
-          file=open("output.txt", "a"))
-
-
-#############
-# Discord events
-#############
-
-# Perhaps add an optional member join message? 
-@bot.listen()
-async def on_ready():
-    log.info(f"Bot is ready: logged in as {bot.user.name} ({bot.user.id})")
-
-    # Ensure the bot is like actually ready so it won't break. 
-    await bot.wait_until_ready()
-
-
-#############
-# Normal commands
-#############
-@bot.command(name="ping", brief="The bot responds if alive")
-async def cmd_ping(ctx: Context):
-    await ctx.send(f"Pong! :ping_pong: \n :clock1: {0}" .format(round(bot.latency, 1)))
-
-
-@bot.command(name="about", brief="Information about the bot. ")
-async def cmd_about(ctx: Context):
-    embed = discord.Embed(description=String.BOT_ABOUT, imageURL="https://i.imgur.com/04PkeD2.jpeg", )
-    embed.set_author(name="Liem Eldert", url="https://github.com/LiemEldert/", icon_url="https://i.imgur.com/WTKqP4k.png")
-    await ctx.send((String.BOT_ABOUT).format(bot_mention=bot.user.mention, bot_version=__version__))
-    
-
-
-bot.run(BOT_TOKEN)
+BOT_PREFIX = config.get("Bot", "bot_prefix", fallback="!").strip(" ")
+SPECIAL_USERS_IDS = [int(id_) for id_ in loads(config.get("Bot", "special_user_ids", fallback="[]"))]
+CHANNEL_ID = config.get("Bot", "channel_id")
+GUILD_ID = config.get("Bot", "guild_id")
+LOG_ID = config.get("Bot", "log_channel_id")
+LANGUAGE = config.get("Bot", "language")
+log.info(f"Special users: {', '.join([str(a) for a in SPECIAL_USERS_IDS])}")
--- zoidbergbot/localization.py
+++ zoidbergbot/localization.py
@@ -31,109 +31,36 @@ sublime with Ayu doesn't work, for instance.
 ▐▐▐▐▐▐▗▚▘  ▖ ▄▜▝▖▌▘  ▗ ▝                    ▘       ▗▚▚▚▝▞▞▖
 ▘▘▝▝ ▘▌   ▗ ▘▘▝▝▝▝ ▘                 ▘   ▘     ▘     ▘▌▘▘▘▝
 """
-
-import discord
+import logging
+from zoidbergbot.config import LANGUAGE
 from typing import Optional
-from random import choice
-from discord import Member, Guild, TextChannel, Message, PermissionOverwrite, Role, CategoryChannel, Reaction
-from discord.ext.commands import Bot, Context, check_any, CheckFailure
-from discord import RawReactionActionEvent
-from discord.errors import HTTPException
-from zoidbergbot.state import state
-from zoidbergbot.config import *
-from zoidbergbot.strings import get_string, String
-from zoidbergbot.utilities import generate_id, generate_code
-from datetime import datetime
-from io import BytesIO
-from discord.ext import commands
-import os
-
-__version__ = "1.0 RELEASE CANDIDATE"
+from json import loads
 
 log = logging.getLogger(__name__)
-bot = Bot(
-    command_prefix=BOT_PREFIX
-)
-
-# "!conf" command
-@bot.command()
-async def conf(ctx, *, message=""):
-    if (len(message) != 0) or (ctx.message.attachments != []):
-
-        now = datetime.now()
-        currentTime = now.strftime("%d/%m %H:%M")
-        userID = ctx.message.author.id
-        server = bot.get_guild(GUILD_ID)
-        member = ctx.message.author
-        # TODO: change this to sqlite3. Not here, in beta, but do that.
-        print(now.strftime("%d/%m %H:%M"), file=open("output.txt", "a"))
-        print(userID, file=open("output.txt", "a"))
-        print(server, file=open("output.txt", "a"))
-        print(member, file=open("output.txt", "a"))
-
-        # Grabs channel IDS from config.
-        # TODO: convert this to use a database per guild id. 
-        channel = bot.get_channel(CHANNEL_ID)
-        logChannel = bot.get_channel(LOG_ID)
-
-        # Embed looks fancy.
-        embed = discord.Embed(description=message)
-        print(message, file=open("output.txt", "a"))
-        # Set date/time as footer of embed
-        embed.set_footer(text=currentTime)
-
-        # Image attachments
-        files = []
-        for file in ctx.message.attachments:
-            fp = BytesIO()
-            await file.save(fp)
-            files.append(discord.File(
-                fp, filename=file.filename, spoiler=file.is_spoiler()))
-            imageURL = ctx.message.attachments[0].url
-            embed.set_image(url=imageURL)
-            print("image" + imageURL)
-
-        # Send embed message to channel
-        await channel.send(embed=embed)
-        embed.set_footer(text=currentTime + str(member))
-        await logChannel.send(embed=embed)
-        # TODO: Change to use strings. 
-        await ctx.send("Your message has been sent!")
 
 
-    else: 
-        await ctx.send(CMD_NO_CONTENT)
-    print("--------------------------------------- END CONFESS ---------------------------------------",
-          file=open("output.txt", "a"))
+with open(f"./data/{LANGUAGE}.json", "r") as strings_:
+    STRINGS = loads(strings_.read())
+
+REQUIRED_STRING_LIST = [
+    "BOT_ABOUT",
+    "CMD_MESSAGE_SENT",
+    "CMD_NO_CONTENT"
+]
+
+# Make sure all required strings are present
+for s in REQUIRED_STRING_LIST:
+    if STRINGS.get(s) is None:
+        raise Exception(f"String {s} missing. Bot cannot open.")
 
 
-#############
-# Discord events
-#############
-
-# Perhaps add an optional member join message? 
-@bot.listen()
-async def on_ready():
-    log.info(f"Bot is ready: logged in as {bot.user.name} ({bot.user.id})")
-
-    # Ensure the bot is like actually ready so it won't break. 
-    await bot.wait_until_ready()
+class String:
+    """
+    helpful.
+    """
+    BOT_ABOUT = "BOT_ABOUT"
+    CMD_NOT_ALLOWED_FOR_USER = "CMD_PERMISSION_ERROR"
 
 
-#############
-# Normal commands
-#############
-@bot.command(name="ping", brief="The bot responds if alive")
-async def cmd_ping(ctx: Context):
-    await ctx.send(f"Pong! :ping_pong: \n :clock1: {0}" .format(round(bot.latency, 1)))
-
-
-@bot.command(name="about", brief="Information about the bot. ")
-async def cmd_about(ctx: Context):
-    embed = discord.Embed(description=String.BOT_ABOUT, imageURL="https://i.imgur.com/04PkeD2.jpeg", )
-    embed.set_author(name="Liem Eldert", url="https://github.com/LiemEldert/", icon_url="https://i.imgur.com/WTKqP4k.png")
-    await ctx.send((String.BOT_ABOUT).format(bot_mention=bot.user.mention, bot_version=__version__))
-    
-
-
-bot.run(BOT_TOKEN)
+def get_string(string_name: str) -> Optional[str]:
+    return STRINGS.get(string_name)
