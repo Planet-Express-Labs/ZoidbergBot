8425765e4046b3dcd57255bd5aa21a1f24cb0bfe
Liem Eldert <liemeldert@protonmail.com>
2021-05-15 12:24:56 -0400

    Resolved some small bugs and updated !ping

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 """
-# if you see an insane mess here, it's supposed to be zoidberg. Probably only works correcly in certain fonts,
+if you see an insane mess here, it's supposed to be zoidberg. Probably only works correcly in certain fonts,
 sublime with Ayu doesn't work, for instance.
+
 ▌▙▚▛▞▚▚▘▌▚▝▄▙▜▜▀▀▙▌▜▟▌▚▌▌▌▚▚▚▚▚▚▚▚▚▚▚▘▌▚▀▖▚▝▖▚▝▖▚▝▖▚▝▖▚▝▖▚▝▖
 ▚▀▄▘▀▖▚▙▞█▛▜▐▐▝▞▞▟▜▐▟▐▚▚▐▐▐▗▚▐▗▚▐▗▚▐▗▚▚▚▚▜▝▞▝▖▌▞▖▌▞▖▚▐▗▚▝▖▚
 ▚▚▐▞▙█▜▐▚▘▞▖▚▗▚▝▖▛▙▐▟▀▖▌▚▐▝▞▞▐▝▞▞▐▝▞▞▞▐▗▚▐▐▞▐▗▘▞▗▝▖▞▝▖▚▗▚▐▝▖
@@ -12,12 +13,12 @@ sublime with Ayu doesn't work, for instance.
 ▛█▙▙▙▛▙▌▌▌█▚▝▐▗▘▌▙▛▞▙▚▚▞▞▞▞▖▖     ▝▝▐▗▚▐ ▘▀   ▚▚▘▞▝▖▚▝▖▖▚▐▗▘
 ▛▙▙▙▙▛▙▌▌▜▝▖▀▖▘▌▞▞▙▜▐▚▖▙▘▌▚▘  ▖▜▘▗   ▞▞▘   ▙   ▌▚▝▞▝▖▌▞▝▖▚▗▘
 █▙▜▟▚▛▙▌▌█▝▞▞▐▝▖▚▛▞▟▌▛▛▌▌▌▌▙       ▝ ▌▞▖ ▝   ▝▗▝▖▌▞▐▗▝▖▌▚▘▖▘
-▙▜▜▟▜▜▙▛▞▟▝▖▞▖▛▟█▜▙▚▛▞▘▌▌▞▞▖▌▖▗ ▘ ▘▗▞▐▐▐▗  ▖▝▗▖▚▘▞▝▖▚▚▝▖▚▝▞▖  __________ _____ _____  ____  ______ _____   _____
-▛█▜▟▛█▟▞▞▟▐▝▖▚▌▀▝▀▝▀▞  ▞▞▞▖▌▌▚▚▗▖▖▜▐▐▝▖▌▌▌▄▗▀▌▞▖▚▝▞▝▖▖▌▞▖▚▗  |___  / __ \_   _|  __ \|  _ \|  ____|  __ \ / ____|
-▛▙▛▙▛▙▙▌▙▀▝           ▐▝▖▌▚▐▐▝▛▀▚▀▞▖▌▌▌▌▞▞▞▞▚▚▗▘▚▚▝▞▝▖▞▗▝▞▝▖    / / |  | || | | |  | | |_) | |__  | |__) | |  __
-▛▛▛▙▛▌▘▘              ▞▞▞▞▞▞▖▙▙▛▞▞▞▐▝▞▐▝▞▖▌▞▚▘▘▘▙▗▚▐▝▞▞▖▌▞▝▖   / /| |  | || | | |  | |  _ <|  __| |  _  /| | |_ |
-█▜▛▌▘     ▝▗         ▗▚▚▐▝▞▄████▞▖▌▌▌▌▌▜▝▞▝▌▌▌▌▗▗▐▐▗▚▚▐▝▞▝▌▀  / /_| |__| || |_| |__| | |_) | |____| | \ \| |__| |
-▛▌▘     ▄▗▐▀▞▚▀▞▚▀▞▖ ▗▚▚▚▚▜█████▞▞▐▝▙▐▐▝▙▚▚▚▚▐ ▌▚▌▌▚▚▘▌▚▚▚▚▘ /_____\____/_____|_____/|____/|______|_|  \_\\_____|
+▙▜▜▟▜▜▙▛▞▟▝▖▞▖▛▟█▜▙▚▛▞▘▌▌▞▞▖▌▖▗ ▘ ▘▗▞▐▐▐▗  ▖▝▗▖▚▘▞▝▖▚▚▝▖▚▝▞▖    __________ _____ _____  ____  ______ _____   _____
+▛█▜▟▛█▟▞▞▟▐▝▖▚▌▀▝▀▝▀▞  ▞▞▞▖▌▌▚▚▗▖▖▜▐▐▝▖▌▌▌▄▗▀▌▞▖▚▝▞▝▖▖▌▞▖▚▗    |___  / __ \_   _|  __ \|  _ \|  ____|  __ \ / ____|
+▛▙▛▙▛▙▙▌▙▀▝           ▐▝▖▌▚▐▐▝▛▀▚▀▞▖▌▌▌▌▞▞▞▞▚▚▗▘▚▚▝▞▝▖▞▗▝▞▝▖      / / |  | || | | |  | | |_) | |__  | |__) | |  __
+▛▛▛▙▛▌▘▘              ▞▞▞▞▞▞▖▙▙▛▞▞▞▐▝▞▐▝▞▖▌▞▚▘▘▘▙▗▚▐▝▞▞▖▌▞▝▖     / /| |  | || | | |  | |  _ <|  __| |  _  /| | |_ |
+█▜▛▌▘     ▝▗         ▗▚▚▐▝▞▄████▞▖▌▌▌▌▌▜▝▞▝▌▌▌▌▗▗▐▐▗▚▚▐▝▞▝▌▀    / /_| |__| || |_| |__| | |_) | |____| | \ \| |__| |
+▛▌▘     ▄▗▐▀▞▚▀▞▚▀▞▖ ▗▚▚▚▚▜█████▞▞▐▝▙▐▐▝▙▚▚▚▚▐ ▌▚▌▌▚▚▘▌▚▚▚▚▘   /_____\____/_____|_____/|____/|______|_|  \_\\_____|
 ▘   ▗▖▜▐▝▞▖▌▌▌▌▌▌▌▘  ▘▖▐▗▚▛█▜██▛▖▌▌▌▌▞▖▙▚▘▌▚▚▚▘▌    ▘▌▌▌▚▘▌▘
   ▗▞▚▐▐▝▞▞▞▞▐▝▞▐     ▗▝ ▚▚▝▌▛▞▟▐▐▝▖▌▌▚▚▞▖▌▌▌▌▞▖      ▘▌▞▞▞▞▖
 ▗▞▚▚▚▚▘▌▌▞▖▌▌▌▀      ▝▗▘▖▚▚▚▐▝▖▌▚▚▜▞▞▞▞▞▞▞▐▝▞▐▐▝       ▝▝▞▐
@@ -32,122 +33,94 @@ sublime with Ayu doesn't work, for instance.
 ▘▘▝▝ ▘▌   ▗ ▘▘▝▝▝▝ ▘                 ▘   ▘     ▘     ▘▌▘▘▘▝
 """
 
-import discord
-import re
-from typing import Optional
-from random import choice
-from discord import Member, Guild, TextChannel, Message, PermissionOverwrite, Role, CategoryChannel, Reaction
-from discord.ext.commands import Bot, Context, check_any, CheckFailure
-from discord import RawReactionActionEvent
-from discord.errors import HTTPException
-from zoidbergbot.config import *
-from zoidbergbot.localization import get_string, String
+# This software is provided free of charge without a warranty - meaning if you're an idiot and somehow
+# blow up your sever, I am not liable or responsible.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import asyncio
 from datetime import datetime
 from io import BytesIO
-from discord.ext import commands
-import os
 
-__version__ = "1.0"
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot, Context
+
+from zoidbergbot.config import *
+from zoidbergbot.localization import get_string, String
+from zoidbergbot.verify import verify_user
+
+# TODO: place these in config.py.
+COGS = ["cogs.confess"]
+permissionLevels = ["dev", "admin"]
+
+# TODO: add to config.
+logging.basicConfig(level=logging.INFO)
+# TODO: change how this is handled.
+__version__ = "1.0 Release Candidate 2"
 
 log = logging.getLogger(__name__)
 bot = Bot(
     command_prefix=BOT_PREFIX
 )
 
-
-def find_url(input):
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s(" \
-            r")<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’])) "
-    return re.search(input, regex)
+bot.load_extension("cogs.confess")
 
 
-# "!conf" command
-@bot.command()
-async def conf(ctx, message):
-    # Error check: skips if message has no content or image
-    if (len(message) != 0) or (ctx.message.attachments != []):
-        now = datetime.now()
-        current_time = now.strftime("%d/%m %H:%M")
-
-        # Obtains User ID and nickname to verify user is in guild/server
-        user_id = ctx.message.author.id
-        server = bot.get_guild(GUILD_ID)
-        # Assigns Discord channel (given channel ID)
-        channel = bot.get_channel(int(CHANNEL_ID))
-        logChannel = bot.get_channel(int(LOG_ID))
-        # last_message_id = ctx.channel.last_message_id
-
-        # TODO: reformat this and add better logging options.
-        print(now.strftime("%d/%m %H:%M"), file=open("../output.txt", "a"))
-        print(user_id, file=open("../output.txt", "a"))
-        print("Guild ID:", GUILD_ID)
-        print(server, file=open("../output.txt", "a"))
-        print("Channel:", CHANNEL_ID, channel, file=open("../output.txt", "a"))
-        print("Log:", LOG_ID, file=open("../output.txt", "a"))
-        print(str(ctx.message.author).encode("utf-8"), file=open("../output.txt", "a"))
-        print(ctx.message)
-
-        # Create embed. They're fancy
-        embed = discord.Embed(description=f"{message}\n\n:id:: \n:card_box::")
-        print(message, file=open("../output.txt", "a"))
-        embed.set_footer(text=f"Pulling info. ")
-        # Send embed.
-        msg = await channel.send(embed=embed)
-        embed = discord.Embed(
-            description=f"{message}  \n\n:id:: {msg.id}")
-        files = []
-        for file in ctx.message.attachments:
-            fp = BytesIO()
-            await file.save(fp)
-            files.append(discord.File(fp, filename=file.filename, spoiler=file.is_spoiler()))
-            imageURL = ctx.message.attachments[0].url
-            embed.set_image(url=imageURL)
-            print("image" + imageURL)
-        url = find_url(message)
-        if url is not None:
-            imageURL = url
-        embed.set_footer(text=f"{current_time}")
-        await msg.edit(embed=embed)
-
-        # log message
-        embed.set_footer(text="%s %s\n%s" % (current_time, str(user_id + 10), ctx.message.author))
-        await logChannel.send(embed=embed)
-        # Inform user their message has been sent
-        await ctx.send("Your message has been sent!")
-
-    # If the message contains no image or text
-    else:
-        await ctx.send("Your message does not contain any content. Message failed.")
-        print("--------------------------------------- END CONFESS ---------------------------------------", file=open("../output.txt", "a"))
-
-
-#############
-# Discord events
-#############
-
-# Perhaps add an optional member join message? 
 @bot.listen()
 async def on_ready():
     log.info(f"Bot is ready: logged in as {bot.user.name} ({bot.user.id})")
 
-    # Ensure the bot is like actually ready so it won't break. 
+    # Really make sure the internal cache is ready
     await bot.wait_until_ready()
+    # await bot.change_presence(status=discord.Status.idle, activity="DM me !conf to confess text/images.")
 
 
-#############
-# Normal commands
-#############
-@bot.command(name="ping", brief="The bot responds if alive")
-async def cmd_ping(ctx: Context):
-    await ctx.send(f"Pong! :ping_pong: \n :clock1: {0}" .format(round(bot.latency, 1)))
+class Main:
 
+    def __init__(self):
+        pass
 
-@bot.command(name="about", brief="Information about the bot. ")
-async def cmd_about(ctx: Context):
-    embed = discord.Embed(description=String.BOT_ABOUT, imageURL="https://i.imgur.com/04PkeD2.jpeg", )
-    embed.set_author(name="Liem Eldert", url="https://github.com/LiemEldert/", icon_url="https://i.imgur.com/WTKqP4k.png")
-    await ctx.send((String.BOT_ABOUT).format(bot_mention=bot.user.mention, bot_version=__version__))
-    
+    @bot.command(name="ping", brief="The bot responds if alive")
+    async def cmd_ping(self, ctx: Context):
+        # Literally just responds with this.
+        await ctx.send(f"Pong! :ping_pong: \n :clock1: {0}" .format(round(bot.latency, 1)))
+
+    @bot.command(name="about", brief="A bit about the bot")
+    async def cmd_about(self, ctx: Context):
+        # Replies with a short blurb of information. I should probably update this later.
+        await ctx.send(get_string(String.BOT_ABOUT).format(bot_mention=bot.user.mention, bot_version=__version__))
+
+    @bot.command(name="set-status",
+                 brief="Set the status of the bot. This saves to a file on the disk. This command is logged, "
+                       "even if you do not have proper permissions.")
+    async def cmd_set_status(self, ctx: Context, *, message=""):
+        # Sets the status of the bot. This will be stored in the config file.
+        print("status:", message)
+        if verify_user(ctx, "dev"):
+            if message is not None:
+                print("status:", message)
+                await ctx.send("Setting status to: " + message)
+                await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=message))
+            else:
+                await ctx.send("Status cannot be sent to a blank string. ")
+        else:
+            await ctx.send("You do not have sufficient permissions to run that command. This event will be logged")
+            print(f"{ctx.message.author} just attempted to use set-status without proper permissions. ",
+                  file=open("permissionLog.txt", "a"))
+
+    @bot.command(name="check-special", brief="Checks if user has any special roles configured within the bot. ")
+    async def cmd_check_perms(self, ctx: Context):
+        message = ""
+        await ctx.send("Checking user permissions... ")
+        for each in permissionLevels:
+            message += f"{each}: {verify_user(ctx, each)}\n"
+        if "True" in message:
+            message += "\nYou are the special one. Be wise, my child."
+        else:
+            message += "\nYou're boring."
+        embed = discord.Embed(description=message)
+        await ctx.send(embed=embed)
 
 
 bot.run(BOT_TOKEN)
