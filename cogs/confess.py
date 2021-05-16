a0d5e9a614c88ec9fc98d03f79db57b871a09881
Liem Eldert <liemeldert@protonmail.com>
2021-05-16 14:27:07 -0400

    Resolved issue where message will not send.

--- cogs/confess.py
+++ cogs/confess.py
@@ -54,49 +54,48 @@ from zoidbergbot.verify import verify_user
 from zoidbergbot.config import *
 
 
-def find_url(input):
+def find_url(url):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s(" \
             r")<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’])) "
-    return re.search(input, regex)
+    return re.search(url, regex)
 
 
 class Confess(commands.Cog):
     """Cog for confessing. As the name makes clear. """
 
+    # noinspection PyShadowingNames
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="conf")
     async def cmd_conf(self, ctx, *, message=""):
-        # Checks if the message actually contains anything worth sending.
         if (len(message) != 0) or (ctx.message.attachments != []):
-            current_time = datetime.now().strftime("%d/%m %H:%M")
+            now = datetime.now()
+            current_time = now.strftime("%d/%m %H:%M")
 
             # Obtains User ID and nickname to verify user is in guild/server
             user_id = ctx.message.author.id
             server = bot.get_guild(GUILD_ID)
             # Assigns Discord channel (given channel ID)
-            print(CHANNEL_ID)
-            print(LOG_ID)
             channel = bot.get_channel(int(CHANNEL_ID))
             logChannel = bot.get_channel(int(LOG_ID))
             # last_message_id = ctx.channel.last_message_id
 
             # TODO: reformat this and add better logging options.
-            print(current_time, file=open("../output.txt", "a"))
+            print(now.strftime("%d/%m %H:%M"), file=open("../output.txt", "a"))
             print(user_id, file=open("../output.txt", "a"))
             print("Guild ID:", GUILD_ID)
             print(server, file=open("../output.txt", "a"))
             print("Channel:", CHANNEL_ID, channel, file=open("../output.txt", "a"))
             print("Log:", LOG_ID, file=open("../output.txt", "a"))
             print(str(ctx.message.author).encode("utf-8"), file=open("../output.txt", "a"))
-            print(ctx.message.content)
-
+            print(ctx.message)
             if get_user_ban(user_id):
                 await ctx.send(
                     "You have been temporarily blacklisted from sending confessions. \nThis could be a "
                     "permanent ban, or simply rate limiting. ")
                 pass
+
             # Create embed. They're fancy
             embed = discord.Embed(description=f"{message}\n\n:id:: \n:card_box::")
             print(message, file=open("../output.txt", "a"))
@@ -116,7 +115,6 @@ class Confess(commands.Cog):
             url = find_url(message)
             if url is not None:
                 imageURL = url
-            await asyncio.sleep(3)
             embed.set_footer(text=f"{current_time}")
             await msg.edit(embed=embed)
 
@@ -126,11 +124,12 @@ class Confess(commands.Cog):
             # Inform user their message has been sent
             await ctx.send("Your message has been sent!")
 
-        # If the message contains no image or text
+            # If the message contains no image or text
         else:
             await ctx.send("Your message does not contain any content. Message failed.")
-        print("--------------------------------------- END CONFESS ---------------------------------------",
-              file=open("../output.txt", "a"))
+            print("--------------------------------------- END CONFESS ---------------------------------------",
+                  file=open("../output.txt", "a"))
+
 
     @cmd_conf.error
     async def conf_error(self, ctx, error):
@@ -139,40 +138,19 @@ class Confess(commands.Cog):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send('You do not have manage_messages permission')
 
-    @commands.command(name="announcement")
-    async def cmd_conf_announcement(self, ctx, message):
-        channel = bot.get_channel(int(CHANNEL_ID))
-        if verify_user(ctx, "admin"):
-            embed = discord.Embed(body=ctx.message, color=0xFF5733, title="Confession Announcement",
-                                  footer=f"confessbot v{get_string('VERSION')}.")
-            await ctx.send(embed=embed)
-
-            async def confirm(m):
-                await ctx.send('Please reply "Confirm" to send the message.')
-
-            try:
-                send = await bot.wait_for('message', check=confirm, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send('Timed out.')
-            if send == "Confirm":
-                await channel.send(embed=embed)
-
-        else:
-            await ctx.send(get_string("BOT_PERMISSION_ERROR"))
-
     @commands.command(name="ban-confessions")
     async def cmd_ban_confessions(self, ctx, *, message=""):
         # TODO: convert everything to docstrings.
         """Bans a user from using confessions. A reason is not yet supported, but might be implemented soon:tm:.
         """
-        if verify_user(ctx, "admin"):
+        if not verify_user(ctx, "admin"):
+            await ctx.send(get_string("CMD_PERMISSION_ERROR"))
+        else:
             if message is None:
                 await ctx.send(get_string("COMMAND_EMPTY_USER_ID"))
             else:
-                add_ban(int(ctx.message.author))
-                await ctx.send(":white_check_mark:")
-        else:
-            await ctx.send(get_string("CMD_PERMISSION_ERROR"))
+                add_ban(message)
+                await ctx.send(f":white_check_mark: Banned {0} from sending confessions.".format(bot.get_user(int(message))))
 
     @commands.command(name="unban-confessions", brief="Unbans a user from confessions. ")
     async def cmd_unban_confessions(self, ctx, *, message=""):
@@ -197,9 +175,12 @@ class Confess(commands.Cog):
         embed = discord.Embed(description="Getting bans, please wait.")
         message = ctx.send(embed=embed)
         if verify_user(ctx, "admin"):
-            for each in get_bans():
-                embed += "\n {0}".format(bot.get_user(each))
-                message.edit(embed=embed)
+            if get_bans() is not None:
+                for each in get_bans():
+                    embed += "\n {0}".format(bot.get_user(each))
+                    message.edit(embed=embed)
+            else:
+                await ctx.send(get_string("NO_RESULTS"))
         else:
             await ctx.send(get_string("CMD_PERMISSION_ERROR"))
 
