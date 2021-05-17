a5c0562bfdb13f337353eba3d94de7ac258f1324
Liem Eldert <liemeldert@protonmail.com>
2021-05-17 02:13:58 -0400

    Updated header and complain-y comment

--- zoidbergbot/localization.py
+++ zoidbergbot/localization.py
@@ -1,3 +1,16 @@
+"""
+8888888888P         d8b      888 888                                    888               888
+      d88P          Y8P      888 888                                    888               888
+     d88P                    888 888                                    888               888
+    d88P    .d88b.  888  .d88888 88888b.   .d88b.  888d888 .d88b.       88888b.   .d88b.  888888
+   d88P    d88""88b 888 d88" 888 888 "88b d8P  Y8b 888P"  d88P"88b      888 "88b d88""88b 888
+  d88P     888  888 888 888  888 888  888 88888888 888    888  888      888  888 888  888 888
+ d88P      Y88..88P 888 Y88b 888 888 d88P Y8b.     888    Y88b 888      888 d88P Y88..88P Y88b.
+d8888888888 "Y88P"  888  "Y88888 88888P"   "Y8888  888     "Y88888      88888P"   "Y88P"   "Y888
+                                                               888
+                                                          Y8b d88P
+                                                           "Y88P"
+"""
 import logging
 from typing import Optional
 from json import loads
@@ -9,6 +22,7 @@ log = logging.getLogger(__name__)
 with open(f"./data/{BOT_LANGUAGE}.json", "r") as read_strings:
     STRINGS = loads(read_strings.read())
 
+# Maybe we shouldn't be handling version through localization...
 REQUIRED_STRING_LIST = [
     "BOT_ABOUT",
     "CMD_PERMISSION_ERROR",
