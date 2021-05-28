0a48231321f7b6c131512451877b6f666500111f
Liem Eldert <liemeldert@protonmail.com>
2021-05-28 16:46:26 -0400

    Removed unused bit of code I never ended up using.

--- zoidbergbot/server_data.py
+++ zoidbergbot/server_data.py
@@ -7,17 +7,19 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 
-class Department(Base):
-    __tablename__ = 'department'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-
-
 class Server(Base):
     __tablename__ = 'server_data'
     guild = Column(Integer, primary_key=True)
     prefix = Column(String)
-    enabled_modules = Column(String)
+    enabled_modules = Column(String) # Reserved for future use
+    # I'll probably change this to something that will allow more then just two levels of permission, but that's later.
+    # If admin_role == 0, then it's just going to go off of each role's permissions.
+    admin_role = Column(bool)
+    mod_role = Column(bool)
+    cooldown = Column(Integer) # Reserved for future use
+    auto_delete = Column(Integer)  # Reserved for future use
+    premium = Column(bool) # Reserved for future use
+
 
 
 engine = create_engine('sqlite:///data/servers.db')
