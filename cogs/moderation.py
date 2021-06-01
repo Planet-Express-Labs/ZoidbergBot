2a3fb126d26a463ea986e4da23f27aca19167e93
Liem Eldert <liemeldert@outlook.com>
2021-06-01 19:20:41 -0400

    Created a *new* and fun cog for people who do bad things and need to be put into time out.

---
+++ cogs/moderation.py
@@ -0,0 +1,28 @@
+from bot import bot
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context
+from zoidbergbot import localization
+
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        bot = self.bot
+
+    @commands.Cog.listener()
+    async def on_command_error(ctx: Context, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(localization.get_string("COMMAND_EMPTY"))
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send(localization.get_string("CMD_PERMISSION_ERROR"))
+
+    @commands.command(name="purge")
+    @commands.has_permissions(manage_messages=True)
+    async def cmd_purge(self, ctx, messages=0, channel=None):
+        if channel is None:
+            channel = ctx.channel
+        await channel.purge(limit=messages)
+
+
+
+
