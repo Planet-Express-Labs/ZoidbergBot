4f3f7a1ad7572d98086adee12ad38675d14cc654
Liem Eldert <liemeldert@protonmail.com>
2021-06-05 14:03:46 -0400

    Removed dead gif

--- data/gifs.py
+++ data/gifs.py
@@ -4,7 +4,6 @@ slap = [
     "https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif",
     "https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif",
     "https://media.tenor.com/images/c6b70091d3643ab80929de2a38343aa5/tenor.gif",
-    "https://media.giphy.com/media/10Am8idu3qBYRy/giphy.gif",
     "https://i.pinimg.com/originals/4e/9e/a1/4e9ea150354ad3159339b202cbc6cad9.gif",
     "https://i.pinimg.com/originals/1c/8f/0f/1c8f0f43c75c11bf504b25340ddd912d.gif",
     "https://i.pinimg.com/originals/b6/e3/9e/b6e39e693be3968d212b0fe5754f85db.gif",
