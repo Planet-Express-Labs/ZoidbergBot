e6bdfa4ed69c6a9b1722bf60e5ff82932a7e1b55
Cyan1970 <85383243+Cyan1970@users.noreply.github.com>
2021-06-05 02:18:48 -0400

    Update gifs.py

--- data/gifs.py
+++ data/gifs.py
@@ -1,16 +1,16 @@
 
 slap = [
-"https://media.tenor.com/images/9e7a8a7473f6535081805f0e85b7a09f/tenor.gif"
-"https://i.pinimg.com/originals/fe/39/cf/fe39cfc3be04e3cbd7ffdcabb2e1837b.gif"
-"https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif"
-"https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif"
-"https://media.tenor.com/images/c6b70091d3643ab80929de2a38343aa5/tenor.gif"
-"file:///C:/Users/trina/AppData/Local/Temp/VW0cOyL.gif" 
-"https://media.giphy.com/media/10Am8idu3qBYRy/giphy.gif"
-"https://i.pinimg.com/originals/4e/9e/a1/4e9ea150354ad3159339b202cbc6cad9.gif"
-"https://i.pinimg.com/originals/1c/8f/0f/1c8f0f43c75c11bf504b25340ddd912d.gif"
-"https://i.pinimg.com/originals/b6/e3/9e/b6e39e693be3968d212b0fe5754f85db.gif"
-"https://media.tenor.com/images/a0c111e14b73a5ff9a876eb6beab6729/tenor.gif"
-"https://media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif?itemid=10426943"
-"https://i.gifer.com/B2Sm.gif"
+"https://media.tenor.com/images/9e7a8a7473f6535081805f0e85b7a09f/tenor.gif",
+"https://i.pinimg.com/originals/fe/39/cf/fe39cfc3be04e3cbd7ffdcabb2e1837b.gif",
+"https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif",
+"https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif",
+"https://media.tenor.com/images/c6b70091d3643ab80929de2a38343aa5/tenor.gif",
+"file:///C:/Users/trina/AppData/Local/Temp/VW0cOyL.gif",
+"https://media.giphy.com/media/10Am8idu3qBYRy/giphy.gif",
+"https://i.pinimg.com/originals/4e/9e/a1/4e9ea150354ad3159339b202cbc6cad9.gif",
+"https://i.pinimg.com/originals/1c/8f/0f/1c8f0f43c75c11bf504b25340ddd912d.gif",
+"https://i.pinimg.com/originals/b6/e3/9e/b6e39e693be3968d212b0fe5754f85db.gif",
+"https://media.tenor.com/images/a0c111e14b73a5ff9a876eb6beab6729/tenor.gif",
+"https://media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif?itemid=10426943",
+"https://i.gifer.com/B2Sm.gif",
 ]
