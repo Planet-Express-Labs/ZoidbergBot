12d4caf7d39d31706de65943f2af56be617356ac
Cyan1970 <85383243+Cyan1970@users.noreply.github.com>
2021-06-05 03:10:21 -0400

    Update gifs.py

--- data/gifs.py
+++ data/gifs.py
@@ -11,10 +11,10 @@ slap = [
 "https://media.tenor.com/images/a0c111e14b73a5ff9a876eb6beab6729/tenor.gif",
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FyfrwZqRvJ5WFy%2Fgiphy.gif&f=1&nofb=1",
 "https://media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif?itemid=10426943",
-"https://i.gifer.com/B2Sm.gif",
+"https://i.gifer.com/B2Sm.gif"
 ]
-cute_animals = [
-cute_dog_anime = {
+
+cute_dog_anime = [
 "https://gifdownload.net/wp-content/uploads/2019/01/anime-dog-slap-gif-.gif",
 "https://cdn2.scratch.mit.edu/get_image/gallery/26447094_170x100.png",
 "https://otakudonblog.files.wordpress.com/2018/06/pskum8e.gif",
@@ -26,10 +26,10 @@ cute_dog_anime = {
 "https://i.pinimg.com/favicons/4ec26d7e86ddc1d731f212bef0ef872f33c4860e4f97b0f260bd5db1.gif?94f88545ca2b44e582dc7bc42e70253e",
 "https://i.pinimg.com/originals/f8/75/d6/f875d60121c86cbe27c54428f8804e27.gif",
 "https://i.gifer.com/LBff.gif",
-"https://i.imgur.com/jm6jDTT.gif",
-}
+"https://i.imgur.com/jm6jDTT.gif"
+  ]
 
-cute_dog_irl = {
+cute_dog_irl = [
 "https://media.tenor.com/images/d7afbeb5c3b3efc48a86eb2c3450ceb8/tenor.gif",
 "https://i.pinimg.com/originals/61/f0/70/61f07041bfef3fb0bd0de2785bf0bc76.gif",
 "https://i2.wp.com/images.onwardstate.com/uploads/2015/05/oie_14175751vZSQRLEn.gif?fit=650%2C408&ssl=1",
@@ -41,10 +41,10 @@ cute_dog_irl = {
 "https://i.chzbgr.com/full/222725/hFCFBFEFE/short-gifs-of-cute-puppies-and-dogs",
 "https://thumbs.gfycat.com/EvergreenTintedArkshell-max-1mb.gif",
 "https://cloudinary-assets.dostuffmedia.com/res/dostuff-media/image/upload/page-image-3732-968887d4-68aa-4149-9f07-0e4f8665ea7d/1440186656.gif",
-"https://cdn.substack.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fc6f44bbc-ce81-4791-bfac-21b5a2689eac_300x300.gif",
-}
+"https://cdn.substack.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fc6f44bbc-ce81-4791-bfac-21b5a2689eac_300x300.gif"
+  ]
 
-cute_cats_anime = {
+cute_cats_anime = [
 "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F68.media.tumblr.com%2Fce9f3c128eae463a729c841dd0519c91%2Ftumblr_o2v48r0M571ufwwv5o1_540.gif&f=1&nofb=1",
 "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgifimage.net%2Fwp-content%2Fuploads%2F2017%2F10%2Fcute-anime-cat-gif-13.gif&f=1&nofb=1",
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2Fdqxj0bInhQFOM%2Fgiphy.gif&f=1&nofb=1",
@@ -54,14 +54,13 @@ cute_cats_anime = {
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F38.media.tumblr.com%2Fb1d2710e1781633b2043c63fb88765bb%2Ftumblr_mhpt7ufkfd1rphs2wo1_500.gif&f=1&nofb=1",
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fimages%2F27c53422e50ecc9d438cda25ae7ec964%2Ftenor.gif%3Fitemid%3D4382416&f=1&nofb=1".
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F33.media.tumblr.com%2F63a5c30a831b897f4074b25445003556%2Ftumblr_no35pqd3nh1upvhr5o1_500.gif&f=1&nofb=1",
-}
+  ]
 
-cute_cats_irl = {
+cute_cats_irl = [
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FrZg1mUbze4lmU%2Fgiphy.gif&f=1&nofb=1",
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FlIgaClvpJdGwg%2Fgiphy.gif&f=1&nofb=1".
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F68.media.tumblr.com%2Fbd08f2aa85a6eb8b7a9f4b07c0807d71%2Ftumblr_ofrc94sG1e1sjmm5ao1_400.gif&f=1&nofb=1",
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0344%2F6469%2Ffiles%2Fcat-gif-loop-ice_grande.gif%3Fv%3D1523984147&f=1&nofb=1",
 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2F3xz2BHFXxvFDYwnA9W%2Fgiphy.gif&f=1&nofb=1",
 "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgif-free.com%2Fuploads%2Fposts%2F2017-03%2F1490894860_cat-wiggle.gif&f=1&nofb=1",
-}
-]
+  ]
