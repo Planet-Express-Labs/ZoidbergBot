b41d8c2c0eaf231cc7e447b499e433961cbaa6c5
Cyan1970 <85383243+Cyan1970@users.noreply.github.com>
2021-06-05 02:08:23 -0400

    Update gifs.py

--- data/gifs.py
+++ data/gifs.py
@@ -1,5 +1,17 @@
 
 slap = [
-    "https://i.pinimg.com/originals/6a/60/d1/6a60d1eaf8c7317f7dfb0a892789c490.gif"
+"https://media.tenor.com/images/9e7a8a7473f6535081805f0e85b7a09f/tenor.gif" ,
+"https://i.pinimg.com/originals/fe/39/cf/fe39cfc3be04e3cbd7ffdcabb2e1837b.gif" ,
+"https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif" ,
+"https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif" ,
+"https://media.tenor.com/images/c6b70091d3643ab80929de2a38343aa5/tenor.gif" ,
+"file:///C:/Users/trina/AppData/Local/Temp/VW0cOyL.gif" , 
+"https://media.giphy.com/media/10Am8idu3qBYRy/giphy.gif" ,
+"https://i.pinimg.com/originals/4e/9e/a1/4e9ea150354ad3159339b202cbc6cad9.gif" ,
+"https://i.pinimg.com/originals/1c/8f/0f/1c8f0f43c75c11bf504b25340ddd912d.gif" ,
+"https://i.pinimg.com/originals/b6/e3/9e/b6e39e693be3968d212b0fe5754f85db.gif" ,
+"https://media.tenor.com/images/a0c111e14b73a5ff9a876eb6beab6729/tenor.gif" ,
+"https://media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif?itemid=10426943" ,
+"https://i.gifer.com/B2Sm.gif" ,
+"https://img.wattpad.com/7147e625951ee0978f59027131f1b1050cf6f591/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f534e7542626170395941745f34673d3d2d3838373837303137352e313633656133333439326338366166333432363833393834313230322e676966" ,
 ]
-
