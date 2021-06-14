c850f9aea3080208c2abb68bc373af2ce9327451
Liem Eldert <liemeldert@protonmail.com>
2021-06-14 15:22:06 -0400

    Added an Embed command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Context
-from dislash import slash_commands, Option, Type, interactions
+from dislash import slash_commands, Option, Type, interactions, Interaction
 
 from bot import guilds
 from zoidbergbot import localization
@@ -17,6 +17,48 @@ class Moderation(commands.Cog):
             await ctx.send(localization.get_string("COMMAND_EMPTY"))
         elif isinstance(error, commands.MissingPermissions):
             await ctx.send(localization.get_string("CMD_PERMISSION_ERROR"))
+
+    @slash_commands.has_permissions(manage_messages=True)
+    @slash_commands.command(
+        name="embed",
+        description="Creates an embed",
+        options=[
+            Option("title", "Creates a title", Type.STRING),
+            Option("description", "Creates a description", Type.STRING),
+            Option("color", "Colors the embed", Type.STRING),
+            Option("image_url", "URL of the embed's image", Type.STRING),
+            Option("footer", "Creates a footer", Type.STRING),
+            Option("footer_url", "URL of the footer image", Type.STRING)
+        ])
+    async def cmd_embed(self, ctx: Interaction):
+        title = ctx.get('title')
+        desc = ctx.get('description')
+        color = ctx.get('color')
+        image_url = ctx.get('image_url')
+        footer = ctx.get('footer')
+        footer_url = ctx.get('footer_url')
+        if color is not None:
+            try:
+                color = await commands.ColorConverter().convert(ctx, color)
+            except:
+                color = discord.Color.default()
+        else:
+            color = discord.Color.default()
+        reply = discord.Embed(color=color)
+        if title is not None:
+            reply.title = title
+        if desc is not None:
+            reply.description = desc
+        if image_url is not None:
+            reply.set_image(url=image_url)
+        pl = {}
+        if footer is not None:
+            pl['text'] = footer
+        if footer_url is not None:
+            pl['icon_url'] = footer_url
+        if len(pl) > 0:
+            reply.set_footer(**pl)
+        await ctx.send(embed=reply)
 
     @slash_commands.has_permissions(manage_messages=True)
     @slash_commands.command(name="purge",
@@ -55,6 +97,8 @@ class Moderation(commands.Cog):
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(localization.get_string("COMMAND_EMPTY"))
+        if isinstance(error, slash_commands.MissingPermissions):
+            await ctx.send(localization.get_string("NO_PERMISSION"))
 
 
 def setup(bot):
