51285bb50887f1ec6c0d3f46a079a6146269a202
Liem Eldert <liemeldert@protonmail.com>
2021-06-14 15:21:55 -0400

    Converted to use slash commands

--- cogs/fun_vol1.py
+++ cogs/fun_vol1.py
@@ -17,7 +17,7 @@ import discord
 from discord.ext import commands
 import art
 
-from bot import bot, BOT_PREFIX, slash
+from bot import bot, slash, guilds
 from zoidbergbot.verify import verify_user
 from data.gifs import *
 from zoidbergbot.localization import get_string
@@ -35,41 +35,61 @@ bad_words = str(base64.b64decode("ZnVjayxiaXRjaCxjdW50LHJhcGUsbmlnZ2VyLG5pZ2dhLG
 words = ["hello"]
 test_guild = 842987183588507670
 
-# update this to not suck
-last_index = 0
-
 
 class FunVol1(commands.Cog):
     """Procrastination but as a module.
     """
+
     def __init__(self, bot):
         self.bot = bot
         # self.slash = slash
+        # Hah, I made it suck in here now!
+        global last_index
+        last_index = 0
 
-    @commands.command(name="big_text")
-    async def cmd_big_text(self, ctx, *, message, style="big"):
+    @slash_commands.command(name="big_text",
+                            description="Repeats your message but ***big***",
+                            guild_ids=guilds,
+                            options=[
+                                Option("text", "What you want to embiggen.", Type.STRING)
+                            ])
+    async def cmd_big_text(self, ctx):
         """Repeats your message but ***big***
         """
-        await ctx.send(f"```{art.text2art(message, font=style)}```")
-        await ctx.message.delete()
+        message = ctx.get("text")
+        style = ctx.get("style")
+        if style is None:
+            style = "big"
 
-    @commands.command(name="random_art")
+        await ctx.send(f"```{art.text2art(message, font=style)}```")
+
+    @slash_commands.command(name="random_art",
+                            description="Sends a random piece of ASCII art from the python art library.",
+                            guild_ids=guilds)
     async def cmd_random_art(self, ctx):
         """Sends a random piece of ASCII art from the python art library.
         """
         message = ctx.message
         await message.delete()
         await ctx.send(f"`{art.randart()}`")
+        # todo: finish this.
         buttons = ActionRow(Button(
             style=ButtonStyle.blurple,
             label=""
         ))
 
-    @commands.command(name="ceaser")
-    async def cmd_ceaser(self, ctx, message, offset=1, inter=None, msg=None):
+    @slash_commands.command(name="ceaser",
+                            guild_ids=guilds,
+                            description="Literally nobody knows what this does. Kai just made it for some reason.\n"
+                                        "something about a ceaser cipher?",
+                            options=[
+                                Option("content", "The content to ceaser. ", Type.STRING)
+                            ])
+    async def cmd_ceaser(self, ctx, offset=1, inter=None, msg=None):
         """Literally nobody knows what this does. Kai just made it for some reason.
         (Silence liem (Next time capitalize.)
         """
+        message = ctx.get("content")
         final = ""
         if type(message) is discord.Message:
             message = ctx.message.content
@@ -107,10 +127,15 @@ class FunVol1(commands.Cog):
         if inter.clicked_button.custom_id == "sub":
             await self.cmd_ceaser(ctx, message, offset - 1, inter, msg)
 
-    @commands.command(name="lonely")
-    async def cmd_lonely(self, ctx, *, message):
+    @slash_commands.command(name="lonely",
+                            guild_ids=guilds,
+                            description="Do you want to be discord friends?",
+                            options=[
+                                Option("content", "What to tell your new discord friend", Type.STRING)
+                            ])
+    async def cmd_lonely(self, ctx):
         # message = ctx.message.content.strip(BOT_PREFIX + "lonely").split(' ')
-        message = message.split(' ')
+        message = ctx.get("content").split(' ')
         if int(len(words)) < 500:
             for i in message:
                 # this is so bad. I don't care tho.
@@ -129,14 +154,17 @@ class FunVol1(commands.Cog):
 
         await ctx.send(final)
 
-    @commands.command(name="get_words")
+    # TODO: make this work very differently.
+    @slash_commands.command(name="get_words",
+                            guild_ids=guilds)
     async def cmd_get_words(self, ctx):
         if verify_user(ctx, "admin"):
             await ctx.send(words)
         else:
             await ctx.send("No ")
 
-    @commands.command(name="reset_words")
+    @slash_commands.command(name="reset_words",
+                            guild_ids=guilds)
     async def cmd_reset_words(self, ctx):
         if verify_user(ctx, "admin"):
             words = ["hello"]
@@ -144,10 +172,16 @@ class FunVol1(commands.Cog):
         else:
             await ctx.send("No ")
 
-    @commands.command(name="blockchain_ceaser")
-    async def cmd_blockchain_ceaser(self, ctx, *, message, inter=None, msg = None):
+    @slash_commands.command(name="blockchain_ceaser",
+                            guild_ids=guilds,
+                            description="Ceaser made even weirder. ",
+                            options=[
+                                Option("content", "What you want to make illegible. ", Type.STRING)
+                            ])
+    async def cmd_blockchain_ceaser(self, ctx, inter=None, msg = None):
         net_value = 0
         prev_values = []
+        message = ctx.get("content")
 
         for i in message:
             val = int(ord(i))
@@ -160,16 +194,22 @@ class FunVol1(commands.Cog):
 
         await ctx.send(final)
 
-    @commands.command(name="slap")
-    async def cmd_slap(self, ctx, person, lastIndex=last_index, loops=0):
-        """Slap your friends for fun. 👏
-        """
+    @slash_commands.command(name="slap",
+                            guild_ids=guilds,
+                            description="Slap your friends for fun. 👏",
+                            options=[
+                                Option("person", "Who you want to slap. ", Type.USER)
+                            ])
+    async def cmd_slap(self, ctx, loops=0):
+
+        global last_index
+        person = ctx.get("person")
         if loops > 10:
             await ctx.send(get_string("UNKNOWN_ERROR") + " TIMEOUT EXCEEDED \n COMMAND: " + ctx.message.content)
             return
         index = random.randint(0, len(slap) - 1)
         timeout = 0
-        while (index != lastIndex | lastIndex != 0) & timeout > 10:
+        while (index != last_index | last_index != 0) & timeout > 10:
             timeout += 1
             index = random.randint(0, len(slap) - 1)
         async with aiohttp.ClientSession() as session:
@@ -177,8 +217,8 @@ class FunVol1(commands.Cog):
                 print(r.status)
                 if r.status == 404:
                     print([index], "is a dead link!")
-                    await self.cmd_slap(ctx, person, lastIndex, loops + 1)
-        lastIndex = index
+                    await self.cmd_slap(ctx, person, last_index, loops + 1)
+        last_index = index
         embed = discord.Embed(title="SLAPPED!", colour=discord.Colour(0x007E5F))
         embed.add_field(name="Slapped:", value=f"{person} (real person!)")
         # TODO: make this use a self hosted CDN instead of random peoples.
@@ -187,7 +227,7 @@ class FunVol1(commands.Cog):
 
     # Ignore this - It's example code to teach Kai.
 
-    # @commands.command(name="simple_interaction")
+    # @slash_commands.command(name="simple_interaction")
     # async def cmd_simple(self, ctx):
     #     buttons = ActionRow(
     #         Button(
@@ -216,26 +256,26 @@ class FunVol1(commands.Cog):
     #                             "%2Fd3MEQYJQpJSKs%2Fgiphy.gif&f=1&nofb=1")
     #         await inter.reply(embed=embed)
 
-    @commands.command(name="discord_friends")
-    async def cmd_discord_friends(self, ctx, person):
-        row_of_buttons = ActionRow(
-            Button(
-                style=ButtonStyle.green,
-                label="Accept",
-                custom_id="green"
-            ),
-            Button(
-                style=ButtonStyle.red,
-                label="Decline",
-                custom_id="red"
-            )
-        )
-
-        # Send a message with buttons
-        msg = await ctx.send(
-            "This message has buttons!",
-            components=[row_of_buttons]
-        )
+    # @slash_commands.command(name="discord_friends")
+    # async def cmd_discord_friends(self, ctx, person):
+    #     row_of_buttons = ActionRow(
+    #         Button(
+    #             style=ButtonStyle.green,
+    #             label="Accept",
+    #             custom_id="green"
+    #         ),
+    #         Button(
+    #             style=ButtonStyle.red,
+    #             label="Decline",
+    #             custom_id="red"
+    #         )
+    #     )
+    #
+    #     # Send a message with buttons
+    #     msg = await ctx.send(
+    #         "This message has buttons!",
+    #         components=[row_of_buttons]
+    #     )
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
