5c1f5b295e92e3d3fc9620c10f2f48a06434dea8
Liem Eldert <liemeldert@protonmail.com>
2021-06-15 17:52:24 -0400

    Added support for environment variables.
    In the future, I might just reference the config file. I also need to add something to be able to have more then one node. It shouldn't be too difficult, but I'm busy with a bunch of things right now.

---
+++ cogs/data/music_nodes.py
@@ -0,0 +1,44 @@
+from zoidbergbot.config import MissingEnvironmentVariableError
+import os
+
+# ugh, this is such a terrible solution.
+
+
+nodes = {"us_east":
+    {
+        "host": "host",
+        "port": 2333,
+        "rest_uri": "http://host.host:2333",
+        "password": "high-security",
+        "identifier": "some creative name",
+        "region": "us_east or some other discord region."
+    }
+}
+
+# Comment this guff out if you don't want to use env variables.
+
+required_vars = []
+
+# ctrl c + ctrl v
+for each in required_vars:
+    # checks each required variable if it exists and raises an exception if it isn't.
+    temp = os.getenv("zoidberg_" + each)
+    if temp is None:
+        raise MissingEnvironmentVariableError
+
+host = os.getenv("zoidberg_music_host")
+port = int(os.getenv("zoidberg_music_port"))
+password = os.getenv("zoidberg_music_password")
+region = os.getenv("zoidberg_music_region")
+
+nodes = {"Primary":
+    {
+        "host": host,
+        "port": port,
+        "rest_uri": f"http://{host}:{port}",
+        "password": password,
+        "identifier": region,
+        "region": region
+    }
+
+}
