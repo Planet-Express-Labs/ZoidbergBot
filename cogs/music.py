5b3dc6c3e2bf6efbc235133b28ad80ac4d1b058f
Liem Eldert <liemeldert@outlook.com>
2021-06-18 02:57:36 -0400

    Did an oopsie, assigned channel as None type rather then None.

--- cogs/music.py
+++ cogs/music.py
@@ -117,8 +117,8 @@ class Music(commands.Cog):
 
         self.bot.loop.create_task(self.start_nodes())
 
-    async def create_connection(self, ctx, channel: None):
-        if not channel:
+    async def create_connection(self, ctx, channel=None):
+        if channel is None:
             try:
                 channel = ctx.author.voice.channel
             except AttributeError:
