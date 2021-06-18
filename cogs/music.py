bcee53fdb1d5239602b23a0aca7684bf4da1aae1
Liem Eldert <liemeldert@protonmail.com>
2021-06-17 22:16:10 -0400

    Updated header

--- cogs/music.py
+++ cogs/music.py
@@ -1,16 +1,18 @@
-"""
-WaveLink Playlist Example.
-Please understand Music bots are complex, and that even this basic example can be daunting to a beginner.
-For this reason it's highly advised you familiarize yourself with discord.py, python and asyncio, BEFORE
-you attempt to write a music bot.
-This example makes use of: Python 3.7, WaveLink(https://github.com/EvieePy/Wavelink) and LavaLink.
-This is a very basic playlist example, which allows per guild playback of unique queues.
-The commands implement very basic logic for basic usage. But allow for expansion. It would be advisable to implement
-your own permissions and usage logic for commands.
-e.g You might like to implement a vote before skipping the song or only allow admins to cmd_stop the player.
-Music bots require lots of work, and tuning. Goodluck.
-If you find any bugs feel free to ping me on discord. @Eviee#0666
-"""
+# 8888888888P         d8b      888 888                                    888               888
+#       d88P          Y8P      888 888                                    888               888
+#      d88P                    888 888                                    888               888
+#     d88P    .d88b.  888  .d88888 88888b.   .d88b.  888d888 .d88b.       88888b.   .d88b.  888888
+#    d88P    d88""88b 888 d88" 888 888 "88b d8P  Y8b 888P"  d88P"88b      888 "88b d88""88b 888
+#   d88P     888  888 888 888  888 888  888 88888888 888    888  888      888  888 888  888 888
+#  d88P      Y88..88P 888 Y88b 888 888 d88P Y8b.     888    Y88b 888      888 d88P Y88..88P Y88b.
+# d8888888888 "Y88P"  888  "Y88888 88888P"   "Y8888  888     "Y88888      88888P"   "Y88P"   "Y888
+# This software is provided free of charge without a warranty.   888
+# This Source Code Form is subject to the terms of the      Y8b d88P
+# Mozilla Public License, v. 2.0. If a copy of the MPL was   "Y88P"
+# this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+# This is designed to be used with Zoidberg bot, however I'm sure it could be adapted to work with your own projects.
+# If there is an issue that might cause issue on your own bot, feel free to pull request if it will improve something.<
 import asyncio
 import datetime
 import itertools
