2f7b1562966493e7e10296ba5377e0b62f841ee7
Liem Eldert <liemeldert@outlook.com>
2021-06-30 18:28:27 -0400

    Cleared out unnecessary imports.

--- cogs/ai.py
+++ cogs/ai.py
@@ -1,13 +1,7 @@
-import base64
-import random
-
-import aiohttp
-import art
-# This is designed to be used with Zoidberg bot, however I'm sure it could be adapted to work with your own projects.
-# If there is an issue that might cause issue on your own bot, feel free to pull request if it will improve something.<3
-import discord
+import re
 from discord.ext import commands
-from dislash import SlashInteraction, ActionRow, Button, slash_commands, Element
+from discord.ext.commands.errors import ConversionError
+from dislash import SlashInteraction, ActionRow, Button, slash_commands, Element, Type, Option
 
 from bot import guilds
 from data.gifs import *
