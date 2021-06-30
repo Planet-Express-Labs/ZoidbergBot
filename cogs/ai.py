1a50e09d9b4ee1fe00cb08c27f54b451c2460937
Liem Eldert <liemeldert@outlook.com>
2021-06-30 18:29:50 -0400

    Added summarize command

--- cogs/ai.py
+++ cogs/ai.py
@@ -22,6 +22,37 @@ class Ai(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
+    
+    @slash_commands.command(name='summarize',
+                            description='Summarizes input text into something shorter using Facebook BartCNN',
+                            testing_guilds=guilds,
+                            options=[
+                                Option('input', 'The text you want to be summarized.', Type.STRING, required=True),
+                                Option('min_length', 'The minimum length of the summary', Type.INTEGER),
+                                Option('max_length', 'The maximum length of the summary', Type.INTEGER),
+                                Option('temperature', 'The temperature of the sampling operation. Higher is closer to uniform probablility.', Type.STRING),
+                                Option('repetition_penalty', 'The higher the value, the higher the penalty is for repeated token.', Type.STRING)
+                            ]
+                            )
+    async def cmd_summarize(self, ctx: SlashInteraction):
+
+        # convert the values into floats since discord does not seem to support them. 
+        try:
+            repetition_penalty = float(repetition_penalty)
+            temperature = float(temperature)
+        except ConversionError:
+            ctx.create_response("The repetition_penalty and temperature must be floats. ")
+
+        # Cut the values down if they are too large.
+        repetition_penalty = trim(repetition_penalty, 100, 0)
+        temperature = trim(temperature, 100, 0)
+
+        text = ctx.get('input')
+        min_length = ctx.get('min_length')
+        max_length = ctx.get('max_length')
+        out = str(NLP.summarize(text, min_length, max_length, repetition_penalty=repetition_penalty, temperature=temperature))
+        ctx.create_response(out)
+
 
 def setup(bot):
     bot.add_cog(Ai(bot))
