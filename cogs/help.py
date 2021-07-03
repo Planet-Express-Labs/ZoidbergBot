1db7ff8994440d58e9420ee5045cd1b06c94e07a
Liem Eldert <liemeldert@outlook.com>
2021-07-03 15:48:25 -0600

    Merged paginator and paginate

--- cogs/help.py
+++ cogs/help.py
@@ -1,94 +1,87 @@
-import base64
-import random
-
-import aiohttp
-import art
 # This is designed to be used with Zoidberg bot, however I'm sure it could be adapted to work with your own projects.
 # If there is an issue that might cause issue on your own bot, feel free to pull request if it will improve something.<3
 import discord
 from discord.ext import commands
-from dislash import SlashInteraction, ActionRow, Button, slash_commands, ButtonStyle
-from zoidbergbot.paginator import Element
+from dislash import *
 
 from bot import guilds
-from data.gifs import *
-from zoidbergbot.localization import get_string
-from zoidbergbot.verify import verify_user
+from zoidbergbot.paginate import Element
+
+
+async def create_menu(ctx: SlashInteraction, menu):
+    # Build buttons
+    button_row_1 = ActionRow(
+        Button(
+            style=ButtonStyle.blurple,
+            emoji="⬆",
+            custom_id="up"
+        ),
+        Button(
+            style=ButtonStyle.green,
+            label="Select",
+            custom_id="select"
+        )
+    )
+    button_row_2 = ActionRow(
+        Button(
+            style=ButtonStyle.blurple,
+            emoji="⬇",
+            custom_id="down"
+        ),
+        Button(
+            style=ButtonStyle.red,
+            label="Back",
+            custom_id="back"
+        )
+    )
+    # Send a message with buttons
+    emb = discord.Embed(
+        title=menu.header,
+        description=f"{menu.long_desc}\n\n{menu.display_elements()}"
+    )
+    msg = await ctx.send(embed=emb, components=[button_row_1, button_row_2])
+
+    # Click manager usage
+
+    on_click = msg.create_click_listener(timeout=60)
+
+    @on_click.matching_id("down")
+    async def down(inter):
+        menu.next_elem()
+
+    @on_click.matching_id("up")
+    async def up(inter):
+        menu.prev_elem()
+
+    @on_click.matching_id("select")
+    async def select(inter):
+        nonlocal menu
+        menu = menu.element
+
+    @on_click.matching_id("back")
+    async def back(inter):
+        nonlocal menu
+        menu = menu.parent
+
+    @on_click.no_checks()
+    async def response(inter):
+        emb.title = menu.header
+        emb.description = f"{menu.long_desc}\n\n{menu.display_elements()}"
+        await inter.reply(embed=emb, type=ResponseType.UpdateMessage)
+
+    @on_click.timeout
+    async def on_timeout():
+        for button in button_row_1.components:
+            button.disabled = True
+        for button in button_row_2.components:
+            button.disabled = True
+        await msg.edit(embed=emb, components=[button_row_1, button_row_2])
 
 
 class Help(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
-
-    async def create_menu(ctx: SlashInteraction, menu):
-        # Build buttons
-        button_row_1 = ActionRow(
-            Button(
-                style=ButtonStyle.blurple,
-                emoji="⬆",
-                custom_id="up"
-            ),
-            Button(
-                style=ButtonStyle.green,
-                label="Select",
-                custom_id="select"
-            )
-        )
-        button_row_2 = ActionRow(
-            Button(
-                style=ButtonStyle.blurple,
-                emoji="⬇",
-                custom_id="down"
-            ),
-            Button(
-                style=ButtonStyle.red,
-                label="Back",
-                custom_id="back"
-            )
-        )
-        # Send a message with buttons
-        emb = discord.Embed(
-            title=menu.header,
-            description=f"{menu.long_desc}\n\n{menu.display_elements()}"
-        )
-        msg = await ctx.send(embed=emb, components=[button_row_1, button_row_2])
-
-        # Click manager usage
-        
-        on_click = msg.create_click_listener(timeout=60)
-        
-        @on_click.matching_id("down")
-        async def down(inter):
-            menu.next_elem()
-        
-        @on_click.matching_id("up")
-        async def up(inter):
-            menu.prev_elem()
-        
-        @on_click.matching_id("select")
-        async def select(inter):
-            nonlocal menu
-            menu = menu.element
-        
-        @on_click.matching_id("back")
-        async def back(inter):
-            nonlocal menu
-            menu = menu.parent
-        
-        @on_click.no_checks()
-        async def response(inter):
-            emb.title = menu.header
-            emb.description = f"{menu.long_desc}\n\n{menu.display_elements()}"
-            await inter.reply(embed=emb, type=ResponseType.UpdateMessage)
-
-        @on_click.timeout
-        async def on_timeout():
-            for button in button_row_1.components:
-                button.disabled = True
-            for button in button_row_2.components:
-                button.disabled = True
-            await msg.edit(embed=emb, components=[button_row_1, button_row_2])
 
     @slash_commands.command(name='music-help',
                             description='Provides information on how to use the music commands. ', 
@@ -143,7 +136,7 @@ class Help(commands.Cog):
             )
             ]
         )
-        self.create_menu(ctx, menu)
+        create_menu(ctx, menu)
 
 def setup(bot):
     bot.add_cog(Help(bot))
