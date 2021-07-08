768dd5dae55c28a5f355244471ec1f7519e65ad1
Liem Eldert <liemeldert@protonmail.com>
2021-07-08 15:58:28 -0400

    Cleaned up

--- cogs/file_tools.py
+++ cogs/file_tools.py
@@ -1,11 +1,21 @@
-import discord
+# 8888888888P         d8b      888 888                                    888               888
+#       d88P          Y8P      888 888                                    888               888
+#      d88P                    888 888                                    888               888
+#     d88P    .d88b.  888  .d88888 88888b.   .d88b.  888d888 .d88b.       88888b.   .d88b.  888888
+#    d88P    d88""88b 888 d88" 888 888 "88b d8P  Y8b 888P"  d88P"88b      888 "88b d88""88b 888
+#   d88P     888  888 888 888  888 888  888 88888888 888    888  888      888  888 888  888 888
+#  d88P      Y88..88P 888 Y88b 888 888 d88P Y8b.     888    Y88b 888      888 d88P Y88..88P Y88b.
+# d8888888888 "Y88P"  888  "Y88888 88888P"   "Y8888  888     "Y88888      88888P"   "Y88P"   "Y888
+# This software is provided free of charge without a warranty.   888
+# This Source Code Form is subject to the terms of the      Y8b d88P
+# Mozilla Public License, v. 2.0. If a copy of the MPL was   "Y88P"
+# this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+# This is designed to be used with Zoidberg bot, however I'm sure it could be adapted to work with your own projects.
+# If there is an issue that might cause issue on your own bot, feel free to pull request if it will improve something.<3
 from io import BytesIO
 from discord.ext import commands
-from dislash import *
-
 from cogs.data import NLP
-from bot import bot
 from zoidbergbot.paginate import *
 
 
@@ -14,7 +24,7 @@ async def summarize_text(text):
     return str(await nlp.summarize(text))
 
 
-async def summary(channel, message, inter):
+async def summary(message, inter):
     text = []
     for file in message.attachments:
         bytes_io = BytesIO()
@@ -32,7 +42,7 @@ async def summary(channel, message, inter):
     await inter.edit(embed=embed)
 
 
-async def txt_file(file, message):
+async def txt_file(message):
     channel = message.channel
     await channel.send(
         "TXT file detected.",
@@ -61,7 +71,7 @@ class FileTools(commands.Cog):
         labels = ["summary", "content"]
         option = [option.value for option in inter.select_menu.selected_options]
         first_option = option[0]
-        # Let's eliminate edgecases to an alarming degree!
+        # Let's eliminate edge-cases to an alarming degree!
         if option not in labels and len(option) == 1 and len(option[0] > 10) and first_option[10].type in range(9):
             options = str([option.value for option in inter.select_menu.selected_options][0]).split(',')
             print(options)
@@ -69,10 +79,10 @@ class FileTools(commands.Cog):
             label = options[0]
             message_id = int(options[1])
             channel_id = int(options[2])
-            channel = bot.get_channel(channel_id)
+            channel = self.bot.get_channel(channel_id)
             message = await channel.fetch_message(message_id)
             if label == "summary":
-                await summary(channel, message, inter)
+                await summary(message, inter)
             if label == "content":
                 file = message.attachments[0]
                 bytes_io = BytesIO()
@@ -86,7 +96,7 @@ class FileTools(commands.Cog):
     async def on_message(self, message):
         for file in message.attachments:
             if ".txt" in file.filename:
-                await txt_file(file, message)
+                await txt_file(message)
 
 
 def setup(bot):
