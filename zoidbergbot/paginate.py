64a32c72b98397654ecdba3512c2cd792af14024
Liem Eldert <liemeldert@outlook.com>
2021-07-28 18:51:50 -0400

    added progress bar fuction

--- zoidbergbot/paginate.py
+++ zoidbergbot/paginate.py
@@ -7,6 +7,26 @@ import textwrap
 # https://github.com/EQUENOS/slash-commands-example-bot/blob/main/pagination.py
 # Many thanks to EQUENOS.
 
+
+async def progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    return f'\r{prefix} |{bar}| {percent}% {suffix}'
+
+
 class Menu:
     def __init__(self):
         self.pages = []
