caa984771f133c7370a83486a6469363303f1dde
Liem Eldert <liemeldert@protonmail.com>
2021-10-23 23:49:07 -0400

    Updated header

--- bot.py
+++ bot.py
@@ -1,18 +1,6 @@
-# 8888888888P         d8b      888 888                                    888               888
-#       d88P          Y8P      888 888                                    888               888
-#      d88P                    888 888                                    888               888
-#     d88P    .d88b.  888  .d88888 88888b.   .d88b.  888d888 .d88b.       88888b.   .d88b.  888888
-#    d88P    d88""88b 888 d88" 888 888 "88b d8P  Y8b 888P"  d88P"88b      888 "88b d88""88b 888
-#   d88P     888  888 888 888  888 888  888 88888888 888    888  888      888  888 888  888 888
-#  d88P      Y88..88P 888 Y88b 888 888 d88P Y8b.     888    Y88b 888      888 d88P Y88..88P Y88b.
-# d8888888888 "Y88P"  888  "Y88888 88888P"   "Y8888  888     "Y88888      88888P"   "Y88P"   "Y888
-# This software is provided free of charge without a warranty.   888
-# This Source Code Form is subject to the terms of the      Y8b d88P
-# Mozilla Public License, v. 2.0. If a copy of the MPL was   "Y88P"
-# this file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-# This is designed to be used with Zoidberg bot, however I'm sure it could be adapted to work with your own projects.
-# If there is an issue that might cause issue on your own bot, feel free to pull request if it will improve something.<3
+# This software is provided free of charge without a warranty. 
+# This Source Code Form is subject to the terms of the Mozilla Public License, 
+# v. 2.0. If a copy of the MPL was this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import discord
 import os
--- jre-13.bat
+++
@@ -1,7 +0,0 @@
-Rem This switches your PATH variable within windows to jdk-13. Of course, this doesn't work with anything outside of Windows. 
-
-@echo off
-set JAVA_HOME=C:\Program Files\Java\jdk-13
-set Path=%JAVA_HOME%\bin;%Path%
-echo Java 13 activated.
-PAUSE
